474a821c63414c197b6dce5d54a064c37deff705
ivorypjh <ivorypjh@gmail.com>
2023-10-17 14:39:58 +0900

    node test

---
+++ test.spec.js
@@ -0,0 +1,9 @@
+describe('Default Test Set', () => {
+    it('test 1 should be passed', () => {
+        console.log('test1 passed')
+    })
+
+    it('test 2 should be passed', () => {
+        console.log('test2 passed')
+    })
+})
